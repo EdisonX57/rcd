0d4fbd7ae9f61cf5a023f0a932a6b4b01fa7c62b
Edison <jairestupinan853@gmail.com>
2022-07-13 17:51:33 -0500

    archivos nuevos añadidos

---
+++ funcion.js
@@ -0,0 +1,12 @@
+document.getElementById("btn").onclick = function(){
+    document.getElementById("seccion").style.display = "none";
+}
+
+document.getElementById("servicios").onclick = function(){
+    document.getElementById("seccion").style.display = "block";
+}
+
+document.getElementById("formulario").onclick = function(){
+    document.getElementById("seccion").style.display = "block";
+}
+
---
+++ funcion2.js
@@ -0,0 +1,13 @@
+document.getElementById("btn").onclick = function(){
+    document.getElementById("seccion").style.display = "none";
+}
+
+document.getElementById("servicios").onclick = function(){
+    document.getElementById("seccion").style.display = "block";
+}
+
+document.getElementById("seccion2").onclick = function(){
+    document.getElementById("seccion").style.display = "block";
+}
+
+
